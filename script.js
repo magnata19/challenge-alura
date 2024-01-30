38125fe001704473d01a9afffd20d1bd68b8dcb7
Davidson <dehpacifico@gmail.com>
2024-01-30 19:16:34 -0300

    alterando botao e iniciando funcao de copiar

--- script.js
+++ script.js
@@ -2,23 +2,33 @@ const criptografar = () => {
   let textCriptografia = document.getElementById("criptografar-text");
   let text = textCriptografia.value;
 
-  let nenhumaMensagem = document.querySelector('.resultado')
-  nenhumaMensagem.removeChild(nenhumaMensagem.lastChild)
+  let nenhumaMensagem = document.querySelector(".resultado");
+  let divCopy = document.querySelector("para-copiar")
+  nenhumaMensagem.removeChild(nenhumaMensagem.lastChild);
 
-  let button = document.createElement('button')
+  let button = document.createElement("button");
   button.textContent = "Copiar";
-  button.style.height = "20px"
-  button.style.backgroundColor = "#0a3871"
-  button.style.border = "none"
-  button.style.outline = "none"
-  button.style.color = "#fff"
-  button.style.width = "70%"
-  button.style.margin = "0 auto"
 
-  button.addEventListener('click', () => {
-    console.log('clicou')
-  })
-  
+  button.style.backgroundColor = "#0a3871";
+  button.style.border = "none";
+  button.style.outline = "none";
+  button.style.color = "#fff";
+  button.style.width = "70%";
+  button.style.margin = "0 auto";
+  button.style.padding = "15px 25px";
+  button.style.fontSize = "18px";
+  button.style.borderRadius = "24px";
+  button.style.cursor = "pointer";
+  button.style.marginTop = "150%";
+
+  button.addEventListener("click", () => {
+    let textoParaCopiar = document.createElement("textarea");
+    textoParaCopiar.value = nenhumaMensagem.textContent;
+    document.body.appendChild(textoParaCopiar)
+    textoParaCopiar.select();
+    document.execCommand('copy');
+    document.body.removeChild(textoParaCopiar);
+  });
 
   text = text.replace(/e/g, "enter");
   text = text.replace(/i/g, "imes");
@@ -26,14 +36,31 @@ const criptografar = () => {
   text = text.replace(/u/g, "ufat");
   text = text.replace(/a/g, "ai");
   nenhumaMensagem.innerHTML = text;
-  nenhumaMensagem.appendChild(button)
+  nenhumaMensagem.appendChild(button);
 };
 
 const descriptografar = () => {
-  let nenhumaMensagem = document.querySelector('.resultado')
+  let nenhumaMensagem = document.querySelector(".resultado");
 
   let textDescriptografia = document.getElementById("criptografar-text");
   let text = textDescriptografia.value;
+
+  let button = document.createElement("button");
+  button.textContent = "Copiar";
+  button.style.backgroundColor = "#0a3871";
+  button.style.border = "none";
+  button.style.outline = "none";
+  button.style.color = "#fff";
+  button.style.width = "70%";
+  button.style.margin = "0 auto";
+  button.style.padding = "15px 25px";
+  button.style.fontSize = "18px";
+  button.style.borderRadius = "24px";
+  button.style.cursor = "pointer";
+
+  button.addEventListener("click", () => {
+    console.log("clicou");
+  });
 
   text = text.replace(/enter/g, "e");
   text = text.replace(/imes/g, "i");
@@ -42,8 +69,9 @@ const descriptografar = () => {
   text = text.replace(/ai/g, "a");
 
   nenhumaMensagem.innerHTML = text;
+  nenhumaMensagem.appendChild(button);
 };
 
 const copiar = () => {
-  console.log('copiou')
-}
+  console.log("copiou");
+};
